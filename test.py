c30444be6685037692231a28fcdc038843f41563
Krzysztof <krzysztof.kapitan@gmail.com>
2017-06-03 13:01:12 +0200

    Labels and logging

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ contributors_per_step = 10
 max_depth = 2
 
 def contributors_for_name(name, client):
+    print "Finding contributors for " + name
+
+    print "\t\tFetching " + str(repos_per_step) + " best ranked repos..."
 
     repos = []
     for repo in client.get_user(name).get_repos():
@@ -16,6 +19,10 @@ def contributors_for_name(name, client):
     repos.sort(key=lambda x: x.stargazers_count, reverse=True)
     repos = repos[:repos_per_step]
 
+    print "\t\tDone!"
+
+    print "\t\tFetching " + str(contributors_per_step) + " most common contributors"
+
     contributors_frequency = defaultdict(int)
     for repo in repos:
         for contributor in repo.get_contributors():
@@ -23,6 +30,9 @@ def contributors_for_name(name, client):
                 contributors_frequency[contributor.login] += 1
 
     best_contributors = sorted(contributors_frequency.iteritems(), key=lambda (k,v): v, reverse=True)
+
+    print "\t\tDone!"
+
     return map(lambda (k, v): k, best_contributors[:contributors_per_step])
 
 def network_for_user(name, network, depth, client):
@@ -39,13 +49,13 @@ def network_for_user(name, network, depth, client):
 network = {}
 g = Github("tsiss", "qwerty123")
 
-network_for_user("kkapitan", network, 1, g)
+network_for_user("KrauseFx", network, 1, g)
 
 G=nx.Graph()
 for (name, contributors) in network.iteritems():
     for contributor in contributors:
         G.add_edge(name, contributor)
 
-nx.draw(G)
+nx.draw(G, with_labels=True)
 plt.savefig("simple_path.png")
 plt.show()
